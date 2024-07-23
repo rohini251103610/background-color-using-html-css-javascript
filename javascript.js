461f05957175448ee2130612f7f22215c8b6b76c
Rohini.R <rohinirachelr@gmail.com>
2024-07-23 17:17:49 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+function bt() {
+    document.getElementById("cont").style.backgroundColor = "red";
+    document.getElementById("span").textContent="red" 
+    document.getElementById("span").style.color="red";
+}
+
+function col() {
+    document.getElementById("cont").style.backgroundColor = "blue";
+    document.getElementById("span").textContent="blue" 
+    document.getElementById("span").style.color="blue";
+}
+function color() {
+    document.getElementById("cont").style.backgroundColor = "lightgreen";
+    document.getElementById("span").textContent="lightgreen" 
+    document.getElementById("span").style.color="lightgreen";
+}
+function col1() {
+    document.getElementById("cont").style.backgroundColor = "lightseagreen";
+    document.getElementById("span").textContent="lightseagreen" 
+    document.getElementById("span").style.color="lightseagreen";
+}
